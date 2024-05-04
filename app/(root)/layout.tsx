6b96637e692b8b71f49cb39c028a20a9d39caa62
awais512 <raza.awais25@gmail.com>
2024-05-04 21:12:42 +0100

    Mobile navbar and Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import { MobileNavbar } from "@/components/mobile-navbar";
+import { Sidebar } from "@/components/sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isloggedIn = { firstName: "Awais", lastName: "Raza" };
+
   return (
-    <main>
-      Sidebar
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={isloggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="Logo" width={30} height={30} />
+          <div>
+            <MobileNavbar user={isloggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
